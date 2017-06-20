98783827ae122371fd21861c5c059fd65dc3f900
tsujimochi <touzirou1908@gmail.com>
2017-06-20 16:30:08 +0900

    06 ボタンを動作させてみよう

--- JavaScript/dotinstall/pwd_generator_js_v3/default.js
+++ JavaScript/dotinstall/pwd_generator_js_v3/default.js
@@ -1,10 +1,28 @@
 (function(){
     'use strict';
 
+    // スライダー変更
     var slider = document.getElementById('slider');
     var label = document.getElementById('label');
-
     slider.addEventListener('change', function(){
         label.innerHTML = this.value;
     });
+
+    // パスワード生成
+    var btn = document.getElementById('btn');
+    var result = document.getElementById('result');
+    function getPassword(){
+        result.value = 'f8asdf8asdf';
+    }
+
+    btn.addEventListener('click', function(){
+        getPassword();
+    });
+
+    result.addEventListener('click', function(){
+        this.select();
+    });
+
+    getPassword();
+
 })();
