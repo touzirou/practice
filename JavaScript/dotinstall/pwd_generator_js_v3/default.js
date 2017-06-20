3202c3b612b7fec8cf35f741a0db4f1bd89dc849
tsujimochi <touzirou1908@gmail.com>
2017-06-20 16:45:08 +0900

    08 記号や数字を含めてみよう

--- JavaScript/dotinstall/pwd_generator_js_v3/default.js
+++ JavaScript/dotinstall/pwd_generator_js_v3/default.js
@@ -1,20 +1,32 @@
 (function(){
     'use strict';
 
-    // スライダー変更
     var slider = document.getElementById('slider');
     var label = document.getElementById('label');
+    var btn = document.getElementById('btn');
+    var result = document.getElementById('result');
+    var numbers = document.getElementById('numbers');
+    var symbols = document.getElementById('symbols');
+
     slider.addEventListener('change', function(){
         label.innerHTML = this.value;
     });
 
-    // パスワード生成
-    var btn = document.getElementById('btn');
-    var result = document.getElementById('result');
     function getPassword(){
-        var seed = 'abcdefghijklmnopqrstuvwxyz';
+        var seed_letters = 'abcdefghijklmnopqrstuvwxyz';
+        var seed_numbers = '0123456789';
+        var seed_symbols = '!#$%&()';
+        var seed;
         var len = slider.value;
         var pwd = '';
+
+        seed = seed_letters + seed_letters.toUpperCase();
+        if(numbers.checked){
+            seed += seed_numbers;
+        }
+        if(symbols.checked){
+            seed += seed_symbols;
+        }
 
         while(len--){
             pwd += seed[Math.floor(Math.random() * seed.length)];
