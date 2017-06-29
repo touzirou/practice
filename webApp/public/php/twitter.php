3c4962162f68cea10607109f2cd3d31944d46f26
tsujimochi <touzirou1908@gmail.com>
2017-06-29 11:17:56 +0200

    Ajax使ってみた

--- webApp/public/php/twitter.php
+++ webApp/public/php/twitter.php
@@ -3,63 +3,21 @@
 
 $get_url = "http://twitter.nantonaku-rt58.com/get";
 $search_url = "http://twitter.nantonaku-rt58.com/search";
+$data = [];
+$url = "";
 
 // POSTするデータ
-
 if(!empty($_POST["condition"])){
-
-$data = array('condition' => $_POST["condition"], 'count' => $_POST["count"]);
-
-
-$content = http_build_query($data);
-
-$content_length = strlen($content);
-
-$options = array('http' => array(
-
-    'method' => 'POST',
-
-    'header' => "Content-Type: application/x-www-form-urlencoded\r\n"
-
-            . "Content-Length: $content_length",
-
-    'content' => $content));
-
-$response = file_get_contents($search_url, false, stream_context_create($options));
+    $data = array('condition' => $_POST["condition"], 'count' => $_POST["count"]);
+    $url = $search_url;
 }else{
-
-$data = array('screen_name' => $_POST["screen_name"], 'count' => $_POST["count"]);
-
-
-$content = http_build_query($data);
-
-$content_length = strlen($content);
-
-$options = array('http' => array(
-
-    'method' => 'POST',
-
-    'header' => "Content-Type: application/x-www-form-urlencoded\r\n"
-
-            . "Content-Length: $content_length",
-
-    'content' => $content));
-
-$response = file_get_contents($get_url, false, stream_context_create($options));
+    $data = array('screen_name' => $_POST["screen_name"], 'count' => $_POST["count"]);
+    $url = $get_url;
 }
-?>
-
-<!DOCTYPE html>
-<html lang='ja'>
-  <head>
-    <meta charset='utf-8'>
-    <title>get</title>
-  </head>
-  <body>
-    <p>
-    <?php
-    	print_r(str_replace("\n", '<br>', $response));
-    ?>
-    </p>
-  </body>
-</html>
+$content = http_build_query($data);
+$content_length = strlen($content);
+$options = array(   'http' => array(
+                  'method' => 'POST',
+                  'header' => "Content-Type: application/x-www-form-urlencoded\r\n". "Content-Length: $content_length",
+                 'content' => $content));
+print(file_get_contents($url, false, stream_context_create($options)));
